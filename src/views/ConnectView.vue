518bc89faef1fbc6df1a684620cb1a2c4154ab44
montajebi <montajebi.ali@gmail.com>
2024-02-11 19:44:38 +0330

    first commit

---
+++ src/views/ConnectView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="connect">
+    <w3m-button />
+  </div>
+</template>
+
+<style>
+.connect {
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
